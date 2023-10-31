31c25edeb3f1e727bac91fe23d0e5abf25bbc8ac
John Jaramillo <jaramillor03@gmail.com>
2023-10-31 01:06:23 -0500

    channel list

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -33,6 +33,7 @@ import {
   SelectTrigger,
   SelectValue
 } from "@/components/ui/select"
+import { useEffect } from "react";
 
 const formsSchema = z.object({
   name: z.string().min(1, {
@@ -47,19 +48,28 @@ const formsSchema = z.object({
 });
 
 export const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModal()
+  const { isOpen, onClose, type, data } = useModal()
   const router = useRouter();
   const params = useParams();
 
   const isModalOpen =  isOpen && type === "createChannel"
+  const { channelType } = data;
 
   const form = useForm({
     resolver: zodResolver(formsSchema),
     defaultValues:{
       name: "",
-      type:  ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     }
   });
+
+  useEffect(() => {
+    if(channelType) {
+      form.setValue("type", channelType)
+    }else {
+      form.setValue("type", ChannelType.TEXT);
+    }
+  }, [channelType, form]);
 
   const isLoading = form.formState.isSubmitting;
 
