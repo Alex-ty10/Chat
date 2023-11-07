7d1f4b34d1bbf54ffc51730f7f14c7d5077a37da
John Jaramillo <jaramillor03@gmail.com>
2023-11-07 17:50:07 -0500

    chat component

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,10 +1,16 @@
 "use client";
 
-import { Member, Message, Profile } from "@prisma/client";
-import { ChatWelcome } from "./chat-welcome";
-import { useChatQuery } from "@/hooks/use-chat-query";
-import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
+import { format } from "date-fns"
+import { Member, Message, Profile } from "@prisma/client";
+import { Loader2, ServerCrash } from "lucide-react";
+
+import { useChatQuery } from "@/hooks/use-chat-query";
+
+import { ChatWelcome } from "./chat-welcome";
+import { ChatItem } from "./chat-item";
+
+const DATA_FORMAT = "d MMM yyyy, HH:mm";
 
 type MessageWithMemberWithProfile = Message &{
   member: Member & {
@@ -83,9 +89,19 @@ export const ChatMessages = ({
           {data?.pages?.map((group, i) => (
             <Fragment key={i}>
               {group.items.map((message: MessageWithMemberWithProfile) => (
-                <div key={message.id}>
-                  {message.content}
-                </div>
+                <ChatItem
+                  key={message.id}
+                  id={message.id}
+                  member={message.member}
+                  currentMember={member}
+                  content={message.content}
+                  fileUrl={message.fileUrl}
+                  deleted={message.deleted}
+                  timestamp={format(new Date(message.createdAt), DATA_FORMAT)}
+                  isUpdated={message.updatedAt !== message.createdAt}
+                  socketUrl={socketUrl}
+                  socketQuery={socketQuery}
+                />
               ))}
             </Fragment>
           ))}
